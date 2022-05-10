97064c4f3a21a6f9fbb386a2a6382ba4b6b098cf
kcoste <karinacoste@yahoo.es>
2022-05-10 12:24:58 +0200

    First commit

---
+++ src/components/example.vue
@@ -0,0 +1,11 @@
+<template></template>
+
+<script>
+const hola = 3;
+if (hola === 3) {
+}
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    {{ chekboxTodos }}
+    {{ select1 }}
+    <p></p>
+    <input
+      type="checkbox"
+      id="checkbox"
+      value="uno"
+      v-model="chekboxTodos"
+      @input="addSelect(selectSelect1)"
+    />
+    <label for="Uno">Uno</label>
+    <div>
+      <select v-model="select1[0]">
+        <option value="volvo">Volvo</option>
+        <option value="saab">Saab</option>
+        <option value="mercedes">Mercedes</option>
+        <option value="audi">Audi</option>
+      </select>
+    </div>
+    <input type="checkbox" id="checkbox2" value="dos" v-model="chekboxTodos" />
+    <label for="Dos">Dos</label>
+    <div>
+      <select v-model="select1[1]">
+        <option value="sss" selected>Volvo</option>
+        <option value="saab">Saab</option>
+        <option value="mercedes">Mercedes</option>
+        <option value="audi">Audi</option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Prueba",
+  data() {
+    return {
+      chekboxTodos: [],
+      select1: ["volvo"],
+    };
+  },
+  methods: {
+    addSelect(select) {
+      return select;
+    },
+  },
+};
+</script>
+
+<style></style>
